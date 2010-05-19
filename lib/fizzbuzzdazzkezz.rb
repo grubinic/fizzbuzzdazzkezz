e3f75f04d8957efa511284a6c20dccfe9ac24a8c
Gorana <Gorana@mao>
2010-05-19 23:32:26 +0200

    added method Range#to_fizzbazzdazzkezz_a, removed "\n" from Integer#fizzbazz

--- lib/fizzbuzzdazzkezz.rb
+++ lib/fizzbuzzdazzkezz.rb
@@ -9,17 +9,30 @@ class Range
 =end
 
   def print_fizzbuzzdazzkezz(hash = {7 => :Dazz, 5 => :Buzz, 3 => :Fizz , 9 => :Kezz})
-    self.each do |i|
-      str = ''
-      hash.sort.each do |key, value|
-        str += value.to_s if i.modulo(key).zero?
-      end
-      str = i if str.empty?
-      puts str
-    end
+    puts to_fizzbuzzdazzkezz_a(hash)
   end
+  
+=begin rdoc
+  Usage: array = (1..100).to_fizzbuzzdazzkezz_a(hash) 
+
+  By default, the following Hash is presented:
+      {3 => :Fizz, 5 => :Buzz, 7 => :Dazz , 9 => :Kezz}
+  We use symbols as it is cheaper than using strings.
+=end
+  def to_fizzbuzzdazzkezz_a(hash = {7 => :Dazz, 5 => :Buzz, 3 => :Fizz , 9 => :Kezz})
+    result = []
+    self.each do |i|
+      str = ''     
+      hash.sort.each do |key, value|
+        str << value.to_s if i.modulo(key).zero?
+      end
+      result << (str.empty? ? i.to_s : str)     
+    end
+    return result
+  end  
 
   alias print_fbdk print_fizzbuzzdazzkezz
+  alias to_fbdk_a to_fizzbuzzdazzkezz_a
 end # End of class Range
 
 class Integer
@@ -41,7 +54,7 @@ Sample:
     hash.to_a.sort.each do |key, value|
       results << value.to_s if modulo(key).zero?
     end
-    results.empty? ?  to_s + "\n" : results + "\n"
+    results.empty? ? to_s : results
   end
   
   alias fizzbuzzdazzkezz fizzbuzz 
